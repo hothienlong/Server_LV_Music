3b7df0c0624b4753437ad94895cb4eb94a2a7425
oohoangoopro <long.ho460@hcmut.edu.vn>
2021-01-24 23:53:35 +0700

    api modify song

--- modify_song.php
+++ modify_song.php
@@ -3,13 +3,18 @@
     require("response.php");
     require('songmodel.php');
 
+    $song_id = $_POST['song_id'];
     $name = $_POST['name'];
     $image = $_POST['image'];
     $song_link = $_POST['song_link'];
     $mv_link = $_POST['mv_link'];
     $lyric = $_POST['lyric'];
 
-    if (strlen($name) <= 0){
+    if (strlen($song_id) <= 0){
+        echo json_encode(new Response(false , "Id bài hát rỗng" ,[]));
+        return;
+    }
+    else if (strlen($name) <= 0){
         echo json_encode(new Response(false , "Tên bài hát rỗng" ,[]));
         return;
     } else if (strlen($song_link) <= 0){
@@ -17,16 +22,17 @@
         return;
     }
 
-    $query = "CALL add_song_proc('$name','$image','$song_link','$mv_link','$lyric');";
+    $query = "CALL modify_song_proc('$song_id','$name','$image','$song_link','$mv_link','$lyric');";
 
     $data = mysqli_query($con , $query);
     $array = [];
   
+
     if($data){
         $row = mysqli_fetch_assoc($data);
-        array_push($array, new Song($row['last_id'] ,$name,$image,$song_link,$mv_link,$lyric) );
+        array_push($array, new Song($song_id,$name,$image,$song_link,$mv_link,$lyric));
         echo json_encode(new Response(true , null ,$array ));
     }else{
-        echo json_encode(new Response(false , "Thêm thất bại" ,[]));
+        echo json_encode(new Response(false , "Cập nhật thất bại" ,[]));
     }
 ?>
